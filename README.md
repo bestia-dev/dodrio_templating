0b79dc65ebbcc591c3ed421dc0948f136a67f3b7
Luciano <luciano.bestia@gmail.com>
2020-01-10 15:05:51 +0700

    xml_html

--- README.md
+++ README.md
@@ -5,16 +5,28 @@
 I don't know. There is so much confusion and choice around javascript/wasm and html templating.  
 I tried to use typed-html. It is macro based. You write a jsx like syntax inside rust functions.  
 It looks really nice to code html directly inside rust.  
-But for every small change I have to recompile the code. That is not very fast in Rust.  
+But for every small change I have to recompile the code. And that is not very fast in Rust.  
 It looks like the intellisense (RLS or rust-analyzer) have a hard time to understand macros like this.  
 When something goes wrong, the error msg is just terrible.
 
-There are a lot of other templating libraries. Maybe I should use one. But I don't know how to embed that in dodrio.  
+There are a lot of other templating libraries. Maybe I should use one.  
+But I don't know how to embed that in dodrio.  
 So I am thinking to try and make something really simple.  
 
-## 1.from html to dodrio
+## Separate two step develpment time and runtime
 
-Dodrio build the vdom with only 2 objects: nodes and attributes.  
-```rust
-use dodrio::builder::*;
+In development time I want the possiblity to quickly modify the html and css code.  
+Then just refresh the page (partialy refresh) and the result should be visible.  
+The process of rust compiling and then starting the app from start is too slow.  
+When dealing with beauty there are a lot of small iterations.  
+The html and css files will be on the server. There is easy to modify them.  
+The app will always download the new file and then interpret it to dodrio vdom.  
+
+But in runtime this is slow.  
+The same html file is possible to generate a file with rust code, that can be compiled and embeded inside the app. For this I will use the build.rs functionality of the rust compiler. I will build one file with rust code for every html. And then this files will be integrated into the code. And after that they will be compiled.  
+
+## ChangeLog
+
+2020-01-10 I succeded to parse the html and create the dodrio vdom.
+
 
--- src/lib.rs
+++ src/lib.rs
@@ -59,15 +59,24 @@
 //endregion
 
 //region: extern and use statements
-use console_error_panic_hook;
+//use console_error_panic_hook;
 use dodrio::bumpalo::{self, Bump};
+use dodrio::builder::*;
 use unwrap::unwrap;
 use wasm_bindgen::prelude::*;
 use dodrio::{Node, Render, RenderContext};
 //use wasm_bindgen::JsCast; //don't remove this. It is needed for dyn_into.
 use web_sys::{console};
-use dodrio::builder::*;
 //endregion
+
+use quick_xml::Reader;
+use quick_xml::events::Event;
+use std::str;
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 ///simple console write with a string
 fn log1(x: &str) {
@@ -90,7 +99,24 @@ pub fn wasm_bindgen_start() {
     let div_for_virtual_dom = unwrap!(document.get_element_by_id("div_for_virtual_dom"));
 
     // Construct a new rendering component.
-    let rrc = RootRenderingComponent::new();
+    let mut rrc = RootRenderingComponent::new();
+
+    //experimenting with xml/html fragments
+    rrc.respbody = r#"<div id="my_project">
+    firstdiv
+        <h1 id="hello"> hahahah </h1>
+        <h2 id="lala"> lalala </h2>
+        jjjjjj
+        <h3 >333333</h3>
+        <div>
+        dddd
+        <p>pppp</p>
+        <p>pppp</p>
+        </div>
+        testfromdiv
+    </div>
+    "#
+    .to_owned();
 
     // Mount the component to the `<body>`.
     let vdom = dodrio::Vdom::new(&div_for_virtual_dom, rrc);
@@ -112,60 +138,122 @@ impl RootRenderingComponent {
 impl Render for RootRenderingComponent {
     #[allow(clippy::panic)]
     fn render<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
-        //experimenting with xml/html fragments
-        let s = r#"<div id="my_project">
-            <h1 id="hello"> hahahah </h1>
-            <h2 id="lala"> lalala </h2>
-        </div>
-        "#;
-
-        //parse to nodes (element,text) and attributes
-        let doc = unwrap!(roxmltree::Document::parse(&s));
         let bump = cx.bump;
-        let n1 = ElementBuilder::new(bump, "p");
-        let mut vt = vec![];
-
-        for node in doc.descendants() {
-            if node.is_element() {
-                vt.push(text(
-                    bumpalo::format!(in bump, "{:?} at {}",
-                    node.tag_name(),
-                    doc.text_pos_at(node.range().start))
-                    .into_bump_str(),
-                ));
-                vt.push(br(bump).finish());
-                for att in node.attributes() {
-                    vt.push(text(
-                        bumpalo::format!(in bump, "{:?} = {}",
-                        att.name(),
-                        att.value()
-                        )
-                        .into_bump_str(),
-                    ));
-                    vt.push(br(bump).finish());
-                }
-                let txt = unwrap!(node.text()).trim();
-                if !txt.is_empty() {
-                    vt.push(text(
-                        bumpalo::format!(in bump, "{:?} = \"{}\" ",
-                        "text",
-                        txt
-                        )
-                        .into_bump_str(),
-                    ));
-                    vt.push(br(bump).finish());
-                }
-            } else {
-                vt.push(text(
-                    bumpalo::format!(in bump, "ajoj. some other type of node {}",
-                    ""
-                    )
-                    .into_bump_str(),
-                ));
-                vt.push(br(bump).finish());
-            }
-        }
-        let n1 = n1.children(vt);
-        n1.finish()
+        let node = parse_xml_create_node(&self.respbody, &bump);
+        //return
+        node
     }
+}
+
+pub fn parse_xml_create_node<'a>(xml_html: &str, bump: &'a Bump) -> Node<'a> {
+    //parse to nodes (element,text) and attributes
+    let mut id_num = 0;
+
+    struct ChildParent {
+        id: usize,
+        parent_id: usize,
+    }
+    //create a dummy node
+    let name = bumpalo::format!(in bump, "{}",
+ "dummy")
+    .into_bump_str();
+    let eee = ElementBuilder::new(bump, name);
+    let mut root_node = eee.finish();
+    //
+    let mut vec_child_parent: Vec<ChildParent> = Vec::new();
+    let mut vec_elem = Vec::new();
+    let mut vec_children = Vec::new();
+    let mut reader = Reader::from_str(xml_html);
+    reader.trim_text(true);
+
+    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
+    loop {
+        let mut buf = Vec::new();
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                id_num += 1;
+                //maybe it must be inside the bump
+                let name = bumpalo::format!(in bump, "{}",
+            str::from_utf8(e.name()).unwrap())
+                .into_bump_str();
+                log1(&format!("START id_num {} name {}", id_num, name));
+                let mut eee = ElementBuilder::new(bump, name);
+                for attx in e.attributes() {
+                    let att = unwrap!(attx);
+                    let key = bumpalo::format!(in bump, "{}",
+            str::from_utf8(&att.key).unwrap())
+                    .into_bump_str();
+                    let value = bumpalo::format!(in bump, "{}",
+            str::from_utf8(&att.value).unwrap())
+                    .into_bump_str();
+                    log1(&format!("key {} value {}", &key, &value));
+                    //wow! because of the dot concatenation fancy programming style
+                    //the variable is moved and then returned. Terrible for non dot concat style.
+                    eee = eee.attr(&key, &value);
+                }
+                //this must be a parent, because the sibling is already finished and poped
+                let mut parent_id = 0;
+                if vec_child_parent.len() > 0 {
+                    parent_id = unwrap!(vec_child_parent.last()).id;
+                }
+                log1(&format!("parent_id {}", &parent_id));
+                vec_elem.push(eee);
+                vec_child_parent.push(ChildParent {
+                    id: id_num,
+                    parent_id: parent_id,
+                });
+                vec_children.push(bumpalo::collections::Vec::new_in(bump));
+            }
+            Ok(Event::End(ref _e)) => {
+                //let name = str::from_utf8(&e.name()).unwrap();
+                let mut epop = unwrap!(vec_elem.pop());
+                let ch = unwrap!(vec_children.pop());
+                //add the children
+                epop = epop.children(ch);
+                let node = epop.finish();
+                log1(&format!("END {:?}", &node));
+                //now add me to my parent
+                let ch_par = unwrap!(vec_child_parent.pop());
+                if ch_par.parent_id == 0 {
+                    //this is the end of the root element
+                    root_node = node;
+                    break;
+                } else {
+                    //fond parent (only one) and add it to him
+                    let mut i = 0;
+                    for x in &vec_child_parent {
+                        if x.id == ch_par.parent_id {
+                            //the child function moves the elem out of the vector.
+                            //that is not allowed. I must found a workaround
+                            //vec_elem[i].child(node);
+                            vec_children[i].push(node);
+                            log1(&format!("added me to my parent{}", ""));
+                            break;
+                        }
+                        i += 1;
+                    }
+                }
+            }
+            Ok(Event::Text(e)) => {
+                let txt = e.unescape_and_decode(&reader).unwrap();
+                //text is also a children, but it cannot have children
+                let txt = bumpalo::format!(in bump, "{}",txt).into_bump_str();
+                let node = text(&txt);
+                log1(&format!("TEXT {:?}", &txt));
+                //add me to my parent
+                let last_el = unwrap!(vec_children.last_mut());
+                last_el.push(node);
+            }
+            Ok(Event::Eof) => break, // exits the loop when reaching end of file
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (), // There are several other `Event`s we do not consider here
+        }
+        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        buf.clear();
+    }
+
+    log1(&format!("{:?}", &root_node));
+
+    //return
+    root_node
 }
